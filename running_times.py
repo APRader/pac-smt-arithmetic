21906cb6e115ae191b58d4fe1b7c6e6d39ff114d
32703513 <ap.rader@yahoo.com>
2020-05-14 14:47:27 +0100

    start of SMT-LIB benchmark script

--- running_times.py
+++ running_times.py
@@ -1,13 +1,12 @@
 import motor_util
 import time
 import pac
-import pandas as pd
 import matplotlib.pyplot as plt
 
 KB_COMPRESSION = 1000000  # compression factor for knowledge base, larger than amount of data points in any profile
-EXAMPLE_COMPRESSION = 100
+EXAMPLE_COMPRESSION = 10
 VALIDITY = 0.5
-NO_OF_QUERIES = 10
+NO_OF_QUERIES = 1
 z3_vars = motor_util.set_up_variables()
 
 tic = time.perf_counter()
@@ -29,20 +28,21 @@ tac = time.perf_counter()
 print(f"Created {len(examples)} inequalities in {tac - tuc:0.1f} seconds.")
 
 running_times = []
-for i in range(1, len(z3_vars) + 1):
-    queries = pac_object.generate_linear_queries(NO_OF_QUERIES, 1, i)
+for i in range(1, 90001, 5000):
+    queries = pac_object.generate_linear_queries(NO_OF_QUERIES, 10)
     current_times = []
     for query in queries:
         tac = time.perf_counter()
-        decision, prop_valid = pac_object.decide_pac(examples, query, VALIDITY)
+        decision, prop_valid = pac_object.decide_pac(examples[:i], query, VALIDITY)
         tec = time.perf_counter()
         current_times.append(tec - tac)
     print(f"Processed {NO_OF_QUERIES} queries for {i} variables in {sum(current_times):0.1f} seconds")
     running_times.append(sum(current_times) / len(current_times))
-plt.plot(range(1, len(running_times) + 1), running_times)
-plt.xlabel('Number of variables')
+#plt.plot(range(1, len(running_times) + 1), running_times)
+plt.plot(range(0, 90000, 5000), running_times)
+plt.xlabel('Number of examples')
 plt.ylabel('Running time (s)')
-plt.title(f"{len(examples)} examples")
+plt.title(f"10 queries")
 plt.savefig(f"running_times.png")
 plt.show()
 tac = time.perf_counter()
---
+++ smt_lib.py
@@ -0,0 +1,39 @@
+from z3 import *
+import pac
+import numpy as np
+
+NO_OF_EXAMPLES = 10
+
+f = open("bmwlin_20_5_1.inter.bmc_k100.smt2", "r")
+lines = f.readlines()
+
+variables = []
+
+for line in lines:
+    if line.startswith("(declare-fun"):
+        var_name = line.split()[1]
+        type_name = line.split()[3]
+        if type_name == "Bool)":
+            variables.append(Bool(var_name))
+        if type_name == "Real)":
+            variables.append(Real(var_name))
+
+f.close()
+
+f = open("assertions.smt2", "r")
+query = f.read()
+f.close()
+
+knowledge_base = [And(-1 <= variable, variable <= 1) for variable in variables if is_real(variable)]
+
+pac_learner = pac.PAC(variables, knowledge_base)
+
+no_of_real_variables = sum(list(map(is_real, variables)))
+no_of_bool_variables = sum(list(map(is_bool, variables)))
+bool_samples = [True, False]
+examples = []
+
+for _ in range(NO_OF_EXAMPLES):
+    random_reals = np.random.uniform(-1, 1, no_of_real_variables)
+    random_bools = np.random.choice(bool_samples, no_of_bool_variables)
+
